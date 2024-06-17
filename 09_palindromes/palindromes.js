9aa9a85e993cba2c108288f1cf43c6b9e35941f2
Ritik Thakur <117618050+rthakur2712@users.noreply.github.com>
2024-06-17 22:42:26 +0530

    Update palindromes.js
    Two-pointer approach

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanedStr = str.replace(" ",'').tolowerCase();
+    let right = cleanedStr.size() - 1;
+    let left = 0;
+    while(left<=right){
+      if(str[left]!==str[right]){
+        return false;
+      }
+      else{
+        left++;
+        right--;
+      }
+    }
+  return true;
 };
 
 // Do not edit below this line
